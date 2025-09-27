1c04a8ac09ce38ba3b5cb44df7b5cc88b53e636a
Prasad <Prasad@DESKTOP-5E4RFKH>
2025-09-27 14:27:44 +0530

    chnages

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import { FaLinkedin, FaYoutube, FaInstagram, FaMapMarkerAlt, FaEnvelope, FaPhone } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
@@ -24,9 +25,9 @@ const Footer = () => {
             <div className="mb-8 sm:mb-0 min-w-[140px]">
             <h3 className="text-lg font-semibold mb-4">Company</h3>
             <ul className="space-y-2">
-                <li><a href="/about" className="text-white/80 hover:text-white transition-colors">About Us</a></li>
+                <li><Link to="/about" className="text-white/80 hover:text-white transition-colors">About Us</Link></li>
                 <li><a href="#" className="text-white/80 hover:text-white transition-colors">Services</a></li>
-                <li><a href="/careers" className="text-white/80 hover:text-white transition-colors">Career</a></li>
+                <li><Link to="/careers" className="text-white/80 hover:text-white transition-colors">Career</Link></li>
                 <li><a href="#" className="text-white/80 hover:text-white transition-colors">Blog</a></li>
               </ul>
             </div>
@@ -65,7 +66,7 @@ const Footer = () => {
         <div className="border-t border-white/20 mt-12 pt-8 flex flex-col md:flex-row justify-between items-center text-center gap-2">
           <p className="text-white/80 text-sm">©2024 GVPL Technologies . All right reserved</p>
           <div className="flex space-x-4">
-            <a href="/privacy-policy" className="text-white/80 hover:text-white text-sm">Privacy Policy</a>
+            <Link to="/privacy-policy" className="text-white/80 hover:text-white text-sm">Privacy Policy</Link>
             <a href="#" className="text-white/80 hover:text-white text-sm">Terms of Services</a>
           </div>
         </div>
