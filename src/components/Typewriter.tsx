9aa8b1b34015f76952174190195d4ab233d516d3
Prasad <Prasad@DESKTOP-5E4RFKH>
2025-09-27 18:56:55 +0530

    chnages in home page

---
+++ src/components/Typewriter.tsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react';
+
+interface TypewriterProps {
+  words: string[];
+  typingSpeed?: number;
+  deletingSpeed?: number;
+  pauseTime?: number;
+  className?: string;
+}
+
+const Typewriter = ({ 
+  words, 
+  typingSpeed = 100, 
+  deletingSpeed = 50, 
+  pauseTime = 2000,
+  className = ""
+}: TypewriterProps) => {
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+  const [currentText, setCurrentText] = useState('');
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  useEffect(() => {
+    const currentWord = words[currentWordIndex];
+    
+    const timeout = setTimeout(() => {
+      if (!isDeleting) {
+        // Typing
+        if (currentText.length < currentWord.length) {
+          setCurrentText(currentWord.substring(0, currentText.length + 1));
+        } else {
+          // Finished typing, wait then start deleting
+          setTimeout(() => setIsDeleting(true), pauseTime);
+        }
+      } else {
+        // Deleting
+        if (currentText.length > 0) {
+          setCurrentText(currentText.substring(0, currentText.length - 1));
+        } else {
+          // Finished deleting, move to next word
+          setIsDeleting(false);
+          setCurrentWordIndex((prev) => (prev + 1) % words.length);
+        }
+      }
+    }, isDeleting ? deletingSpeed : typingSpeed);
+
+    return () => clearTimeout(timeout);
+  }, [currentText, isDeleting, currentWordIndex, words, typingSpeed, deletingSpeed, pauseTime]);
+
+  return (
+    <span className={className}>
+      {currentText}
+    </span>
+  );
+};
+
+export default Typewriter;
